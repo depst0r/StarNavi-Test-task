501a78154bf97a39f6158ef9181c30f61291c57a
depstor <depstor90@gmail.com>
2021-02-14 17:24:19 +0300

    header

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
-import React from react
+import React, { useState } from 'react'
 
 export const App = () => {
-  return<>
-    <h1>React</h1>
+  return <>
+    <div className="container">
+      <div className="row">
+        <div className='wrapper'>
+          <div className="header">
+            <select className="form-select form-select-sm" aria-label=".form-select-sm example">
+              <option selected>Pick Mode</option>
+              <option value="1">One</option>
+              <option value="2">Two</option>
+              <option value="3">Three</option>
+            </select>
+          </div>
+          <div className='field'></div>
+          <div className="history"></div>
+        </div>
+      </div>
+    </div>
   </>
 }
