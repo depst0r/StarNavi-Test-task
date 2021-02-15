54972a062b3e21c37d73301e18da3cee07606d43
depst0r <depstor90@gmail.com>
2021-02-15 16:20:32 +0300

    props-value

--- src/App.jsx
+++ src/App.jsx
@@ -3,43 +3,44 @@ import React, { useState, useEffect } from 'react'
 export const App = () => {
 
   const [mode, setMode] = useState(null)
+  const [test, setTest] = useState('')
 
   const apiBase = url => {
     fetch(url)
-      .then(res => res.json())
-      .then(res => setMode(res))
+    .then(res => res.json())
+    .then(res => setMode(res))
   }
 
   useEffect(() => {
     apiBase('http://demo1030918.mockable.io/')
   }, [])
 
-const uperCase = str => {
-  return str[0].toUpperCase() + str.slice(1)
-}
+  const uperCase = str => {
+    return str[0].toUpperCase() + str.slice(1)
+  }
 
   return <>
     <div className="container">
       <div className="row">
         <div className='wrapper'>
           <div className="header d-flex">
-            <select className="form-select form-select-sm w-25" aria-label=".form-select-sm example">
+            <select className="form-select form-select-sm w-25" onChange={e => setTest(e.target.value)} aria-label="form-select-sm example">
               <option defaultValue>Pick Mode</option>
-              {mode ? Object.keys(mode).map((res, i) => (
+              {mode ? Object.keys(mode).map((res, i) =>( 
                 <option value={res} key={i}>{uperCase(res)}</option>
               )) : null}
             </select>
             <button type="button" className="btn btn-info">START</button>
           </div>
-          <Field />
+          <Field field={test}/>
         </div>
       </div>
     </div>
   </>
 }
 
-const Field = mode => {
-  console.log(mode);
+const Field = field => {
+  console.log(field);
   return <>
   </>
 }
