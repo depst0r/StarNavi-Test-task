c47fa9d735bdbbbf84aff1d2f835b6ad71ac9db7
depst0r <depstor90@gmail.com>
2021-02-15 17:44:54 +0300

    fValue

--- src/App.jsx
+++ src/App.jsx
@@ -15,8 +15,16 @@ export const App = () => {
     apiBase('http://demo1030918.mockable.io/')
   }, [])
 
-  const uperCase = str => {
-    return str[0].toUpperCase() + str.slice(1)
+  // const uperCase = str => {
+  //   return str[0].toUpperCase() + str.slice(1)
+  // }
+
+  const handlerFielValue = (selectorValue, state) => {
+    Object.fromEntries(
+      Object.entries(state).map(([key, value]) => {
+        return [key === selectorValue ? console.log(value) : null]
+      })
+    )
   }
 
   return <>
@@ -24,24 +32,30 @@ export const App = () => {
       <div className="row">
         <div className='wrapper'>
           <div className="header d-flex">
-            <select className="form-select form-select-sm w-25" onChange={e => setSelectChoice(e.target.value)} aria-label="form-select-sm example">
+            <select className="form-select form-select-sm w-25" onChange={e => handlerFielValue(e.target.value, mode)} aria-label="form-select-sm example">
               <option defaultValue>Pick Mode</option>
               {mode ? Object.keys(mode).map((res, i) =>( 
-                <option value={res} key={i}>{uperCase(res)}</option>
+                <option value={res} key={i}>{res}</option>
               )) : null}
             </select>
             <button type="button" className="btn btn-info">START</button>
           </div>
-          <Field field={setSelectChoice} state={mode}/>
+          {/* <Field field={selectChoice} state={mode}/> */}
         </div>
       </div>
     </div>
   </>
 }
 
-const Field = (field, state) => {
-  console.log(field);
-  console.log(state)
+const Field = field => {
+    const handlerFielValue = () => {
+      Object.fromEntries(
+        Object.entries(field.state).map(([key, value]) => {
+          return [key === field.field ? console.log(value) : null]
+        })
+      )
+    }
   return <>
+  <button onClick={() => handlerFielValue()}>hhhh</button>
   </>
 }
