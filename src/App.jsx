ad8d7a4f66569323a230aa16ec53f6079aa68580
depst0r <depstor90@gmail.com>
2021-02-16 17:17:51 +0300

    start-field

--- src/App.jsx
+++ src/App.jsx
@@ -1,74 +1,94 @@
-import React, { useState, useEffect, createElement } from 'react'
-import './style.css'
+import React, { useState, useEffect, createElement } from "react";
+import "./style.css";
 
 export const App = () => {
+  const [mode, setMode] = useState(null);
+  const [selectChoice, setSelectChoice] = useState("");
 
-  const [mode, setMode] = useState(null)
-  const [selectChoice, setSelectChoice] = useState('')
-
-  const apiBase = url => {
+  const apiBase = (url) => {
     fetch(url)
-    .then(res => res.json())
-    .then(res => setMode(res))
-  }
+      .then((res) => res.json())
+      .then((res) => setMode(res));
+  };
 
   useEffect(() => {
-    apiBase('http://demo1030918.mockable.io/')
-  }, [])
+    apiBase("http://demo1030918.mockable.io/");
+  }, []);
 
-
-  return <>
-    <div className="container">
-      <div className="row">
-        <div className='wrapper'>
-          <div className="header d-flex">
-            <select className="form-select form-select-sm w-25" onChange={e => setSelectChoice(e.target.value)} aria-label="form-select-sm example">
-              <option defaultValue>Pick Mode</option>
-              {mode ? Object.keys(mode).map((res, i) =>( 
-                <option value={res} key={i}>{res}</option>
-              )) : null}
-            </select> 
+  return (
+    <>
+      <div className="container">
+        <div className="row">
+          <div className="wrapper">
+            <div className="header d-flex">
+              <select
+                className="form-select form-select-sm w-25"
+                onChange={(e) => setSelectChoice(e.target.value)}
+                aria-label="form-select-sm example"
+              >
+                <option defaultValue>Pick Mode</option>
+                {mode
+                  ? Object.keys(mode).map((res, i) => (
+                      <option value={res} key={i}>
+                        {res}
+                      </option>
+                    ))
+                  : null}
+              </select>
+            </div>
+            <Field field={selectChoice} state={mode} />
           </div>
-          <Field field={selectChoice} state={mode}/>
         </div>
       </div>
-    </div>
-  </>
-}
+    </>
+  );
+};
 
-const Field = field => {
-  const [fieldCounter, setFieldCounter] = useState('')
+const Field = (field) => {
+  const [fieldCounter, setFieldCounter] = useState("");
 
-    const handleFielValue = () => {
-      Object.fromEntries(
-        Object.entries(field.state).map(([key, value]) => {
-          return [key === field.field ? setFieldCounter(value) : null]
-        })
-      )
-    }
+  const handleFielValue = () => {
+    Object.fromEntries(
+      Object.entries(field.state).map(([key, value]) => {
+        return [key === field.field ? setFieldCounter(value) : null];
+      })
+    );
+  };
 
+  const arr = new Array(fieldCounter.field).fill();
 
-    const arr = new Array(fieldCounter.field).fill(fieldCounter.field)
-
-  return <>
-    <button 
-    type="button" 
-    className="btn btn-info"
-    onClick={() => handleFielValue()}
-    >
-      START
+  return (
+    <>
+      <button
+        type="button"
+        className="btn btn-info"
+        onClick={() => handleFielValue()}
+      >
+        START
       </button>
 
-  <div className="field">
-     {arr.map((res, i) => {
-       return <div
-       key={i}
-      data-col={`row - ${i}`}
-      data-row={`col - ${i}`} 
-        className="square" 
-        onMouseOver={e => console.log(e.target.dataset.col, e.target.dataset.row)}>
-    </div>
-     })}
-  </div>
-  </>
-}
+      {fieldCounter ? (
+        <div className="field">
+        {arr.map((res, i) => {
+          return (
+            <>
+            <div
+              key={i}
+              data-col={`row - ${i}`}
+              data-row={`col - ${i}`}
+              className="square"
+              onMouseOver={(e) =>
+                console.log(e.target.dataset.col, e.target.dataset.row)
+              }
+            >
+            </div>
+            </>
+          )
+        })}
+      </div>
+      ) : (
+        <h1>Start</h1>
+      )}
+    </>
+  );
+};
