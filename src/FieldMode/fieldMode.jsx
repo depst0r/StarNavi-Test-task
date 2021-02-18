2c8ec8b3fa12c8554edc881e9bed96b3e0d31ad1
depst0r <depstor90@gmail.com>
2021-02-18 18:24:30 +0300

    coordinates-render-page-all

--- src/FieldMode/fieldMode.jsx
+++ src/FieldMode/fieldMode.jsx
@@ -1,59 +1,59 @@
-import React, { useState } from 'react'
-import { Position } from '../Position/position';
+import React, { useState } from "react";
+import { Position } from "../Position/position";
 
 const squareStyle = {
-    width: "30px",
-    height: "30px",
-    cursor: "pointer",
-  };
+  width: "30px",
+  height: "30px",
+  cursor: "pointer",
+};
 
 export const FieldMode = ({ selection }) => {
-    const [fieldCounter, setFieldCounter] = useState(0);
-    const [position, setPosition] = useState([]);
-  
-    const handleFielValue = () => {
-      setFieldCounter(selection);
-    };
+  const [fieldCounter, setFieldCounter] = useState(0);
+  const [position, setPosition] = useState([]);
 
-    const array = (pos, pos2) => setPosition([...position, {pos, pos2}])
-  
-    const arr = new Array(+fieldCounter).fill();
-  console.log(position)
-    return <>
-        <Position coordinates={position} />
-        <button
-          type="button"
-          className="btn btn-info"
-          onClick={() => handleFielValue()}
-        >
-          START
-        </button>
-        <div className="wrapper">
-          {arr.map((res, i) => {
-            return (
-              <div className="d-flex" data-row={`row - ${i + 1}`}>
-                {arr.map((res, j) => {
-                  return (
-                    <>
-                      <div
-                        className="border border-3"
-                        style={squareStyle}
-                        data-col={`col - ${j + 1}`}
-                        onMouseOver={(e) =>
-                          array(e.target.dataset.col, e.target.parentNode.dataset.row,)
-                          // setPosition({
-                          //   ...position,
-                          //   row: e.target.parentNode.dataset.row,
-                          //   col: e.target.dataset.col,
-                          // })
-                        }
-                      ></div>
-                    </>
-                  );
-                })}
-              </div>
-            );
-          })}
-        </div>
-      </>
+  const handleFielValue = () => {
+    setFieldCounter(selection);
   };
+
+  const changePosition = (row, col) => setPosition([...position, { row, col }]);
+
+  const arr = new Array(+fieldCounter).fill();
+  console.log(position);
+  return (
+    <>
+      <Position coordinates={position} />
+      <button
+        type="button"
+        className="btn btn-info"
+        onClick={() => handleFielValue()}
+      >
+        START
+      </button>
+      <div className="wrapper">
+        {arr.map((res, i) => {
+          return (
+            <div className="d-flex" data-row={`row - ${i + 1}`}>
+              {arr.map((res, j) => {
+                return (
+                  <>
+                    <div
+                      className="border border-3"
+                      style={squareStyle}
+                      data-col={`col - ${j + 1}`}
+                      onMouseOver={(e) =>
+                        changePosition(
+                          e.target.parentNode.dataset.row,
+                          e.target.dataset.col
+                        )
+                      }
+                    ></div>
+                  </>
+                );
+              })}
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
+};
--- src/Position/position.jsx
+++ src/Position/position.jsx
@@ -1,10 +1,26 @@
-import React from 'react'
+import React from "react";
+
+const overflove = {
+  overflow: 'auto',
+  width: '200px',
+  height: '100px',
+  border: '1px solid black',
+}
 
 export const Position = ({ coordinates }) => {
-
-    return<>
-  <div className="form-floating" style={{width: '25%'}}>
-    <textarea className="form-control" value={coordinates.col + ' ' + coordinates.row}></textarea>
-  </div>
-      </>
-  };
+  console.log(coordinates);
+  return (
+    <>
+      <div className="card" style={ overflove}>
+        <div className="card-header">COORDINATES</div>
+        <ul className="list-group list-group-flush">
+          {coordinates ? coordinates.map((res, k) => (
+            <li className="list-group-item" key={k}>{`${res.col} ${res.row}`}</li>
+          )) : (
+            null
+          )}
+        </ul>
+      </div>
+    </>
+  );
+};
