d9e68bdb3add0e6c0a07a6c12e0b09f6f658b083
depst0r <depstor90@gmail.com>
2021-02-19 18:40:25 +0300

    :dollar:

--- src/Position/position.jsx
+++ src/Position/position.jsx
@@ -3,13 +3,13 @@ import React from "react";
 export const Position = ({ coordinates }) => {
   return (
     <>
-      <div className="card overflow-auto" style={{height: '300px'}}>
+      <div className="card overflow-auto" style={{ height: "300px" }}>
         <div className="card-header">COORDINATES</div>
         <ul className="list-group list-group-flush">
           {coordinates
             ? coordinates.map((res, k) => (
                 <li
-                  style={{color: '#929ba5'}}
+                  style={{ color: "#929ba5" }}
                   className="list-group-item"
                   key={k}
                 >{`${res.col} ${res.row}`}</li>
